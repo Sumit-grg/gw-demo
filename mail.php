5b769f130d95072c1a0e636aa0b20db7b3132079
Sumit-grg <65023009+Sumit-grg@users.noreply.github.com>
2022-03-14 11:30:54 +0000

    init

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$formcontent="From: $name \n Message: $message";
+$recipient = "dinoshahi@hotmail.com";
+$subject = "Gurkha Contact Form";
+$mailheader = "From: $email \r\n";
+if (mail($recipient, $subject, $formcontent, $mailheader) ){
+header('Location: ./thankyou.html');
+
+exit();
+
+
+} else{
+  header("Location: contact.html");
+}
+
+
+
+
+?>
---
+++ script.js
@@ -0,0 +1,20 @@
+const checkbox = document.querySelector("input[name=checkbox]");
+const sidemenu = document.getElementById("what");
+
+checkbox.addEventListener('change', function() {
+  if (this.checked) {
+    console.log("Checkbox is checked..");
+    sidemenu.style.display = "block";
+  } else {
+    console.log("Checkbox is not checked..");
+    sidemenu.style.display = "none";
+  }
+});
+
+window.onscroll = function() {
+  if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight - 100) {
+  document.querySelector(".downarrow").style.display = "none";
+  } else {
+    document.querySelector(".downarrow").style.display = "block";
+  }
+}
